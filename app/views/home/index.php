4878790a188b35bb9c8133ae83d704d0e8a4c223
FrxHaikalPlg <haikalashshiddiq12@gmail.com>
2024-04-19 11:55:23 +0700

    Menambah carousel ke home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -12,7 +12,51 @@
     ?>
 <?php endif; ?>
 
-<div class="album py-5 bg-body-tertiary">
+<div id="carouselExampleIndicators" class="carousel slide shadow" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img src="<?= BASEURL; ?>/img/cw1.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; height: 500px;" alt="...">
+      <div class="carousel-caption text-start pb-5 mb-5" >
+      <h1 style="text-shadow: 2px 2px 4px #000000;">Selamat Datang di Perpustakaan Kami</h1>
+      <p style="text-shadow: 2px 2px 4px #000000;">Temukan buku favoritmu di sini.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="<?= BASEURL; ?>/img/cw2.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; height: 500px;" alt="...">
+      <div class="carousel-caption pb-5 mb-5">
+        <h1 style="text-shadow: 2px 2px 4px #000000;">Jelajahi Koleksi Buku Terlengkap</h1>
+        <p style="text-shadow: 2px 2px 4px #000000;">Dari fiksi hingga non-fiksi, semua ada.</p>
+        <p><a class="btn btn-lg btn-primary" href="#">Browse</a></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="<?= BASEURL; ?>/img/cw3.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; height: 500px;" alt="...">
+      <div class="carousel-caption text-end pb-5 mb-5 pr-1">
+        <h1 style="text-shadow: 2px 2px 4px #000000;">Pinjam Buku dengan Mudah</h1>
+        <p style="text-shadow: 2px 2px 4px #000000;">Temukan dan pinjam buku favoritmu tanpa ribet.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div class="album pb-5 bg-body-tertiary">
+  <div class="text-center my-5">
+      <h1 class="fw-bold fst-italic fs-1" >Editor's Choice</h1>
+      <p  >Jelajahi koleksi pilihan editor kami dan temukan buku yang sempurna untuk petualangan membaca Anda selanjutnya.</p>
+  </div>
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
